09162c42dc06b5d8c2262cb3d63d9c706aecc7b2
Unknown <maelle.d46@gmail.com>
2017-12-19 16:30:38 +0100

    main still in progress
    bloublbloublaaa
    
                Stop the dick. 
                I want to get off.
                           \
              
                    -      #####       .|||||.
                          ####  \_    |||||||||
                          ##=-[.].]  ||||||  .
                     --   #(    _\   ||||||   >
                           #   __|  ||||||| -/
                    -      _\   /   ||||||'(
                          (  `-'|   .'      \
                         / \  \/_.-'    | | |
                        /   \____|_)     \ \ \
                  --   |-----.._      `---:.`.\
                 ______(_____   )          \\_\\____ ,--.__
      --    ,--""           /  / `-   .     |)_)    '\     '\
           /  "            /  /    .-'     /          \      '\
         ,/               /__/           .'            '\     |
         | "   "   "     [____)         /                '\,  /
         |           " , =_____`-.   .-'_________________,--""
       - |  "    "    /"/'      /\>-' ( <
         \  "      ",/ /    -  ( <    |\_)
          \   ",",_/,-'        |\_)
       -- -'-;.__:-'                    vk/lament/dp

--- src/controler/Main.java
+++ src/controler/Main.java
@@ -1,8 +1,39 @@
 package controler;
 
-public class Main {
+import agency.RealEstateAgency;
+import agency.Visit;
 
-	public static void main(String[] args) {
+import java.util.Date;
+
+import agency.Notary;
+import person.Person;
+import property.Property;
+
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		 RealEstateAgency krustyKrabAgency = new RealEstateAgency();
+		 Person spongeBobSeller = new Person("Bikini Bottom One","spongeBobi@lovekrustypate.com", "SpongeBob","+330631224568","Pacific Ocean");
+		 Person starfishPatrickBuyer = new Person("Bikini Bottom Two","starfishTrickPa@lovekrustypate.com", "Patrick Wonderful Starfish","+330604468764");
+		 
+		 krustyKrabAgency.registerSeller(spongeBobSeller,new Date(2017,1,27));
+		 
+		 
+		 
+		 
+		 krustyKrabAgency.registerBuyer(,poule);
+		 
+		 
+		// signSaleMandate(Visit vis, Property prop, Date availabilityDate, Date desiredSaleDate, float desiredPrice)
+		
+		
+		
+		
+		
+		
 		
 
 	}
