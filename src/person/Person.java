66e8c6c6d503b4091aee4661d54b583754b1882f
Unknown <maelle.d46@gmail.com>
2017-12-19 15:35:58 +0100

    Person modifier
    Ajout constructeur + modif put on sale property kekechosecommeca

--- src/person/Person.java
+++ src/person/Person.java
@@ -10,19 +10,24 @@ import agency.SalesMandate;
 import agency.Wish;
 import property.Property;
 
-public class Person {
+
+
+public class Person 
+{
 
 	private String address;
 	private String email;
 	private String name;
 	private String phoneNumber;
-	
+
 	private List<SalesAgreement> listSalesAgreement;
 	private List<SalesMandate> 	 listSalesMandate;
 	private List<Wish> 			 listWhishes;
 	private boolean PropertySimilirarToWishAvailable;
-	
+
 	private Notary associatedNotary;
+
+	
 	
 	//Seller
 	public Person(String address, String email, String name, String phoneNumber, String addressNotary)
@@ -31,50 +36,57 @@ public class Person {
 		this.email = email;
 		this.name = name;
 		this.phoneNumber = phoneNumber;
-		
+
 		this.associatedNotary = new Notary(addressNotary, this);
-		
+
 		this.listSalesAgreement = new ArrayList<>();
 		this.listSalesMandate 	= new ArrayList<>();
 		this.listWhishes 		= new ArrayList<>();
-		
+
 	}
-	
+
 	//Buyer
-	public Person(String address, String email, String name, String phoneNumber){
+	public Person(String address, String email, String name, String phoneNumber)
+	{
 		this.address = address;
 		this.email = email;
 		this.name = name;
 		this.phoneNumber = phoneNumber;
-				
+
 		this.listSalesAgreement = new ArrayList<>();
 		this.listSalesMandate 	= new ArrayList<>();
 		this.listWhishes 		= new ArrayList<>();
-		
 	}
-	
-	
-	
+
+
+
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(Object obj) 
+	{
 		Person tmp;
-		
-		try{
+
+		try
+		{
 			tmp = (Person)obj;
 		}
-		catch(ClassCastException e){
+		catch(ClassCastException e)
+		{
 			return false;
 		}
+
 		
 		boolean ret = tmp.getAddress().equals(this.getAddress());
 		if(ret) ret = tmp.getEmail().equals(this.getEmail());
 		if(ret) ret = tmp.getName().equals(this.getName());
 		if(ret) ret = tmp.getPhoneNumber().equals(this.getPhoneNumber());
-		
+
 		return ret;
 	}
-	
-	public void addWish(Wish wish){
+
+
+
+	public void addWish(Wish wish)
+	{
 		for(Wish w : this.listWhishes)
 			if(w.equals(wish)) this.listWhishes.add(wish);
 		//TODO : si propriété similaire en vente, changer this.PropertySimilirarToWishAvailable à true
@@ -83,80 +95,103 @@ public class Person {
 	public void putPropertyOnSale(Property prop, Date availabilityDate, Date desiredSaleDate, float desiredPrice) throws Exception
 	{
 		if(this.associatedNotary == null) throw new Exception("Cannot use putPropertyOnSale because there is no notary(not a seller)");
+		
 		SalesMandate sMand = new SalesMandate(prop, availabilityDate, desiredSaleDate, desiredPrice);
 		prop.setSaleMandate(sMand);
 		this.listSalesMandate.add(sMand);
 	}
-	
-	public boolean removePropertyFromSale(Property prop){
+
+
+
+
+	public boolean removePropertyFromSale(Property prop)
+	{
 		int rankSaleAgreement = -1;
 		boolean isPropertyStillOnSale = false;
-		
-		for(int i = 0; i < this.listSalesMandate.size(); i++){
+
+		for(int i = 0; i < this.listSalesMandate.size(); i++)
+		{
 			if(this.listSalesMandate.get(i).getProperty().equals(prop)) rankSaleAgreement = i;
 		}
-		
-		if(rankSaleAgreement != -1){
+
+		if(rankSaleAgreement != -1)
+		{
 			isPropertyStillOnSale = true;
-			if(!this.listSalesMandate.get(rankSaleAgreement).isSaleState()){
+			if(!this.listSalesMandate.get(rankSaleAgreement).isSaleState())
+			{
 				this.listSalesMandate.remove(rankSaleAgreement);
 				isPropertyStillOnSale = false;
 			}
 		}
-		
+
 		return isPropertyStillOnSale;
 	}
 
-	public String getAddress() {
+
+
+	public String getAddress() 
+	{
 		return address;
 	}
 
-	public String getEmail() {
+	public String getEmail() 
+	{
 		return email;
 	}
-	
-	public String getName() {
+
+	public String getName() 
+	{
 		return name;
 	}
 
-	public String getPhoneNumber() {
+	public String getPhoneNumber() 
+	{
 		return phoneNumber;
 	}
 
-	public void setAddress(String address) {
+	public void setAddress(String address)
+	{
 		this.address = address;
 	}
 
-	public void setEmail(String email) {
+	public void setEmail(String email) 
+	{
 		this.email = email;
 	}
 
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
-	public void setPhoneNumber(String phoneNumber) {
+	public void setPhoneNumber(String phoneNumber)
+	{
 		this.phoneNumber = phoneNumber;
 	}
-	
-	public List<SalesAgreement> getListSalesAgreement() {
+
+	public List<SalesAgreement> getListSalesAgreement() 
+	{
 		return listSalesAgreement;
 	}
 
-	public List<SalesMandate> getListSalesMandate() {
+	public List<SalesMandate> getListSalesMandate() 
+	{
 		return listSalesMandate;
 	}
 
-	public List<Wish> getListWhishes() {
+	public List<Wish> getListWhishes() 
+	{
 		return listWhishes;
 	}
 
-	public Notary getAssociatedNotary() {
+	public Notary getAssociatedNotary() 
+	{
 		return associatedNotary;
 	}
 
-	public void setAssociatedNotary(Notary associatedNotary) {
+	public void setAssociatedNotary(Notary associatedNotary) 
+	{
 		this.associatedNotary = associatedNotary;
 	}
-	
-	
+
+
 }
